713f294fd5d0c7411d97b1acd21c0e960d7f15ca
EmilianoMelendr <>
2021-05-21 02:14:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("______________________________________________________________");
+    System.out.println("|                                                            |");
+    System.out.println("|                      Welcome To Help Desk                  |");
+    System.out.println("|                                                            |");
+    System.out.println("|                                                            |");
+    System.out.println("|                  A Safe Place To Ask For Help              |");
+    System.out.println("|                                                            |");
+    System.out.println("|                                                            |");
+    System.out.println("|                         Type \"0\" To Start                  |");
+    System.out.println("|                                                            |");
+  }
+}
